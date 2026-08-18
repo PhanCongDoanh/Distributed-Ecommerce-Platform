1ab0edab56283ce18f1cb056e95fcdfa9dea6eb8
doanh <pcd20041807@gmail.com>
2026-08-19 05:29:11 +0700

    feat: add OrderRequest DTO

---
+++ src/main/java/com/danny/springe_com/model/dto/OrderRequest.java
@@ -0,0 +1,9 @@
+package com.danny.springe_com.model.dto;
+
+import java.util.List;
+
+public record OrderRequest(
+        String customerName,
+        String email,
+        List<OrderItemRequest> items
+) { }
