82e19c8fc09e8d36a91e6592de8b6e4d1bc5b376
doanh <pcd20041807@gmail.com>
2026-08-25 10:40:59 +0700

    refactor: add @GeneratedValue(strategy = GenerationType.IDENTITY) to class Order

--- src/main/java/com/danny/springe_com/model/Order.java
+++ src/main/java/com/danny/springe_com/model/Order.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(unique = true)
